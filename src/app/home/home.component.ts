24fad36c29cff6fb0c1834d980d3ce4c105ba660
Thashmi03 <thashmigaa.cs20@bitsathy.ac.in>
2024-04-03 17:21:35 +0530

    slider added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  hospitalImages = [
-    { url: 'assets/images/image1.jpg' },
-    { url: 'assets/images/image2.jpg' },
-    { url: 'assets/images/image3.jpg' }
+export class HomeComponent implements OnInit{
+
+  images: string[] = [
+    'assets/img/slider/hos_anime.jpg',
+    'assets/img/slider/hos1.jpg',
+    'assets/img/slider/1.jpg'
   ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.showSlides();
+  }
+
+  showSlides() {
+    let slideIndex = 0;
+    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    
+    setInterval(() => {
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      slides[slideIndex-1].style.display = "block";  
+    }, 4000); // Change slide every 2 seconds
+  }
 }
